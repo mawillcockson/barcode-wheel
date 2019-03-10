77e4f21907c760b7ef0399aeafc2062613ae875f
Matthew Willcockson <matthew@willcockson.us>
2019-03-10 03:20:18 -0500

    Ran black against repository
    Not added to pre-commit hooks.
    
    Yet.
    
    Signed-off-by: Matthew Willcockson <matthew@willcockson.us>

--- insert_barcode.py
+++ insert_barcode.py
@@ -6,25 +6,30 @@ import re
 from collections import namedtuple
 from io import StringIO
 
-zint_output = subprocess.run(shlex.split("zint --direct --filetype=svg --barcode=34 --notext -d '12345678901'"), stdout=subprocess.PIPE)
-zint_barcode = StringIO(zint_output.stdout.decode('utf8'))
+zint_output = subprocess.run(
+    shlex.split("zint --direct --filetype=svg --barcode=34 --notext -d '12345678901'"),
+    stdout=subprocess.PIPE,
+)
+zint_barcode = StringIO(zint_output.stdout.decode("utf8"))
 
 tree = parse(zint_barcode)
 root = tree.getroot()
-g = [x for x in root.getchildren() if x.tag == '{http://www.w3.org/2000/svg}g'][0]
+g = [x for x in root.getchildren() if x.tag == "{http://www.w3.org/2000/svg}g"][0]
 
-barcode = svgwrite.Drawing(filename='read_zint.svg',
-                           size=(root.attrib['width'], int(root.attrib['height']) - 9),
-                           profile='tiny',
-                           preserveAspectRatio='xMidyMid meet',
-                           viewBox=f'0 0 {root.attrib["width"]} {int(root.attrib["height"]) - 9}')  # Magic number to crop text
+barcode = svgwrite.Drawing(
+    filename="read_zint.svg",
+    size=(root.attrib["width"], int(root.attrib["height"]) - 9),
+    profile="tiny",
+    preserveAspectRatio="xMidyMid meet",
+    viewBox=f'0 0 {root.attrib["width"]} {int(root.attrib["height"]) - 9}',
+)  # Magic number to crop text
 
-tag_re = re.compile(r'(?<=svg})\w+$')
+tag_re = re.compile(r"(?<=svg})\w+$")
 
-group = barcode.g(id='barcode', fill=g.attrib['fill'])
+group = barcode.g(id="barcode", fill=g.attrib["fill"])
 
 for elem in g:
-    if 'text' in elem.tag:
+    if "text" in elem.tag:
         continue
 
     tag_name = tag_re.search(elem.tag)
@@ -33,22 +38,26 @@ for elem in g:
 
     tag_name = tag_name.group()
 
-    if tag_name not in ['rect']:
+    if tag_name not in ["rect"]:
         breakpoint()
 
-    Rect = namedtuple('Rect', 'x, y, width, height, fill')
+    Rect = namedtuple("Rect", "x, y, width, height, fill")
 
-    if 'fill' in elem.attrib:
-        fill = elem.attrib.pop('fill')
+    if "fill" in elem.attrib:
+        fill = elem.attrib.pop("fill")
     else:
-        fill = '#000000'
+        fill = "#000000"
 
     rect_tuple = Rect(fill=fill, **elem.attrib)
 
-    if tag_name == 'rect':
-        group.add(barcode.rect(insert=(rect_tuple.x, rect_tuple.y),
-                               size=(rect_tuple.width, rect_tuple.height),
-                               fill=rect_tuple.fill))
+    if tag_name == "rect":
+        group.add(
+            barcode.rect(
+                insert=(rect_tuple.x, rect_tuple.y),
+                size=(rect_tuple.width, rect_tuple.height),
+                fill=rect_tuple.fill,
+            )
+        )
 
 barcode.add(group)
 barcode.save()
